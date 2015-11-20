ca62d7b946eb6878491aca86d2a8efb248d2e093
cdroulers <droulers.christian@gmail.com>
2015-11-19 20:08:53 -0500

    Add quick in memory debates

--- src/Settlr.Api/Controllers/DebatesController.cs
+++ src/Settlr.Api/Controllers/DebatesController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNet.Mvc;
 
 namespace Settlr.Api.Controllers
@@ -6,10 +8,45 @@ namespace Settlr.Api.Controllers
     [Route("debates")]
     public class DebatesController : Controller
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private static readonly IList<Debate> debates = new List<Debate>()
         {
-            return new string[] { "value1", "value2" };
+            new Debate()
+            {
+                Id = 1,
+                Title = "Toilet paper in front or back?",
+                AnswerLeft = "Front",
+                AnswerRight = "Back"
+            }
+        };
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Debate> Get()
+        {
+            return debates;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public Debate Post([FromBody]Debate debate)
+        {
+            debate.Id = debates.Max(x => x.Id) + 1;
+            debates.Add(debate);
+            return debate;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Debate Get(int id)
+        {
+            var debate = debates.FirstOrDefault(x => x.Id == id);
+            if (debate == null)
+            {
+                // Should return 404;
+                throw new Exception("404 not found");
+            }
+
+            return debate;
         }
     }
 }
---
+++ src/Settlr.Api/Domain/Debate.cs
@@ -0,0 +1,13 @@
+namespace Settlr.Api
+{
+    public class Debate
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string AnswerLeft { get; set; }
+
+        public string AnswerRight { get; set; }
+    }
+}
